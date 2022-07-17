0f7817b37ea9b5dd149eae404bea58ccfe05c837
Luan Pinto <luan.nelmo@hotmail.com>
2022-07-17 19:31:45 -0300

    Classe Campo: Abrir

--- src/main/java/projeto/curso/cm/modelo/Campo.java
+++ src/main/java/projeto/curso/cm/modelo/Campo.java
@@ -1,5 +1,7 @@
 package projeto.curso.cm.modelo;
 
+import projeto.curso.cm.excecao.ExplosaoException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,5 +39,31 @@ public class Campo {
         } else {
             return false;
         }
+    }
+
+    void alternarMarcacao() {
+        if (!aberto) {
+            marcado = !marcado;
+        }
+    }
+
+    boolean abrir() {
+
+        if (!aberto && !marcado) {
+            aberto = true;
+
+            if (minado) {
+                throw new ExplosaoException();
+            }
+            if (vizinhancaSegura()){
+                vizinhos.forEach(v -> v.abrir());
+            }
+            return true;
+        }
+        return false;
+    }
+
+    boolean vizinhancaSegura() {
+        return vizinhos.stream().noneMatch(v -> v.minado);
     }
 }
