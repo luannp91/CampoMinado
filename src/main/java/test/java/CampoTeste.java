94bae30180b02757b460a8a7fd7d191df83cddf7
Luan Pinto <luan.nelmo@hotmail.com>
2022-07-17 19:18:36 -0300

    Classe Campo: Adicionar Vizinhos

---
+++ src/main/java/test/java/CampoTeste.java
@@ -0,0 +1,60 @@
+package test.java;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import projeto.curso.cm.modelo.Campo;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CampoTeste {
+
+    private Campo campo;
+
+    @BeforeEach
+    void iniciarCampo() {
+       campo = new Campo(3, 3);
+    }
+
+    @Test
+    void testeVizinhoRealDistancia1Esquerda() {
+        Campo vizinho = new Campo(3, 2);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+
+    @Test
+    void testeVizinhoRealDistancia1Direita() {
+        Campo vizinho = new Campo(3, 4);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+
+    @Test
+    void testeVizinhoRealDistancia1Emcima() {
+        Campo vizinho = new Campo(2, 3);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+
+    @Test
+    void testeVizinhoRealDistancia1Embaixo() {
+        Campo vizinho = new Campo(4, 3);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+
+    @Test
+    void testeVizinhoRealDistancia2Diagonal() {
+        Campo vizinho = new Campo(2, 2);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+
+    @Test
+    void testeNaoVizinho() {
+        Campo vizinho = new Campo(1, 4);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertFalse(resultado);
+    }
+}
